9cfc8180d089e6e7d9d33d702456b62d42841f22
Colby Mullins <42915868+Sharky20@users.noreply.github.com>
2025-05-16 22:30:11 -0400

    Finish exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    if (typeof num == "string") {
+        num = Number(num);
+    }
+    if (num <= 1) {
+        return num;
+    }
+    return fibonacci(num-1) + fibonacci(num - 2);
 
 };
 
