37b63ccb27ca4665b1d8b88da2c99b6687512c16
Colby Mullins <42915868+Sharky20@users.noreply.github.com>
2025-05-16 22:45:34 -0400

    Finish final JS exercise 16
    Been a long road, onto the last project of foundations.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let oldAge = 0;
+    let oldestPerson = {}
+    array.forEach(person => {
+        let age = 0;
+        if (!("yearOfDeath" in person)) {
+            age = new Date().getFullYear() - person.yearOfBirth;
+        }
+        else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        if (age > oldAge) {
+            oldAge = age;
+            oldestPerson = person;
+        }
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
