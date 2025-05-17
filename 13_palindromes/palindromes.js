40339d8f9b607b500af965a46519e13b4b0d5ee9
Colby Mullins <42915868+Sharky20@users.noreply.github.com>
2025-05-16 22:23:28 -0400

    Finish exercises 12 and 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let arr = str.toLowerCase().replaceAll(" ", "").split("");
+    let cleanArr = [];
+    arr.forEach(element => {
+        if ((element >= "A" && element <= "Z") || (element >= "a" && element <= "z") || (element >= "0" && element <= "9")) {
+            cleanArr.push(element);
+        }
+    });
 
+    let left = 0;
+    let right = cleanArr.length - 1;
+
+    for (; left < right; left++, right--) {
+        if (!(cleanArr.at(left) == cleanArr.at(right))) {
+            return false;
+        }
+    }
+    
+    return true;
+    
 };
 
 // Do not edit below this line
